17286078f2a6d9bd92e1636cb4e1695be6149413
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    ali: Build Audio HIDL HAL interfaces
    Change-Id: Ide624d3008739b68a1b4cac347237e72cd4b4b20

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
