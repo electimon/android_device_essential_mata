10503ce6ef5c70193846bce0e6252d33606eceb1
dev-harsh1998 <dev-harsh1998@hotmail.com>
2019-03-03 23:43:41 +0530

    ali: Implement binderized light hal
    * Also add support for attention notification blink pattern which wasn't present in the previous hal.
    
    Change-Id: I3bb5402109d39dd69e4fbc42c2a400e6b8cc00bf
    Signed-off-by: dev-harsh1998 <dev-harsh1998@hotmail.com>

--- device.mk
+++ device.mk
@@ -166,6 +166,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
     android.hardware.keymaster@3.0-service
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.ali
+
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
