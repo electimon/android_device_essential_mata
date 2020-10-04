519a9008a31d767de10459777976ea47be490b1e
electimon <electimon@gmail.com>
2020-10-04 05:48:42 +0000

    ali: Add missing sensor config

--- device.mk
+++ device.mk
@@ -329,6 +329,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \
